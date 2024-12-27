0aa5d4bb89a32c3b481a28865e89a503c8e2e929
rayan2228 <rayanh.dev@gmail.com>
2024-12-27 17:57:22 +0600

    subcategory create

--- src/controller/subcategory.controller.js
+++ src/controller/subcategory.controller.js
@@ -1,7 +1,22 @@
+import { Subcategory } from "../model/subcategory.schema.js";
+import ApiErrors from "../utils/ApiErrors.js";
+import ApiResponse from "../utils/ApiResponse.js";
 import { TryCatch } from "../utils/TryCatch.js";
 
 const createSubcategory = TryCatch(async (req, res) => {
-
+    let { name, slug } = req.body
+    if (!name) {
+        throw new ApiErrors(400, "subcategory name is required")
+    }
+    if (!slug) {
+        slug = name.replaceAll(" ", "-").toLowerCase()
+    }
+    const isName = await Subcategory.findOne({ name })
+    if (isName) {
+        throw new ApiErrors(400, "subcategory name must be unique")
+    }
+    const subcategory = await Subcategory.create({ name, slug })
+    return res.status(201).json(new ApiResponse(201, "subsubcategory created successfully", { subcategory }))
 })
 
 export { createSubcategory }
