cce344269238d7fe8520138469bbae5a6f08f3cf
rayan2228 <rayanh.dev@gmail.com>
2024-12-27 17:52:34 +0600

    category create

--- src/middleware/admin.middleware.js
+++ src/middleware/admin.middleware.js
@@ -3,7 +3,7 @@ import { TryCatch } from "../utils/TryCatch.js";
 export const admin = TryCatch(async (req, _, next) => {
     const user = req.user
     if (user.role === "admin") {
-        next()
+       return next()
     }
     throw new ApiErrors(401, `Invalid Access`);
 })
