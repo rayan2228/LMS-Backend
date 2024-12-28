3ded038ad39d09978fe7d05d823bfd4436d88188
rayan2228 <rayanh.dev@gmail.com>
2024-12-28 16:26:28 +0600

    create subcategory fun

--- src/controller/subcategory.controller.js
+++ src/controller/subcategory.controller.js
@@ -4,19 +4,29 @@ import ApiResponse from "../utils/ApiResponse.js";
 import { TryCatch } from "../utils/TryCatch.js";
 
 const createSubcategory = TryCatch(async (req, res) => {
+    const updates = {};
+    if (req.file) {
+        const thumbnail = req.file;
+        const cloudinaryResult = await cloudinaryUpload(thumbnail.path, name, "subcategory");
+        updates.thumbnail = {
+            public_id: cloudinaryResult.public_id,
+            url: cloudinaryResult.optimizeUrl,
+        };
+    }
     let { name, slug } = req.body
     if (!name) {
         throw new ApiErrors(400, "subcategory name is required")
-    }
-    if (!slug) {
-        slug = name.replaceAll(" ", "-").toLowerCase()
     }
     const isName = await Subcategory.findOne({ name })
     if (isName) {
         throw new ApiErrors(400, "subcategory name must be unique")
     }
-    const subcategory = await Subcategory.create({ name, slug })
-    return res.status(201).json(new ApiResponse(201, "subsubcategory created successfully", { subcategory }))
+    if (!slug) {
+        slug = name.replaceAll(" ", "-").toLowerCase()
+    }
+    updates.name = name
+    updates.slug = slug
+    const subcategory = await Subcategory.create({ updates })
+    return res.status(201).json(new ApiResponse(201, "subcategory created successfully", { subcategory }))
 })
-
 export { createSubcategory }
