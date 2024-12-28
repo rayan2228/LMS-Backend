73c289c604096d238f5f5160911531d8a4e4e55e
rayan2228 <rayanh.dev@gmail.com>
2024-12-28 17:56:36 +0600

    create category and update category logic done

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -1,20 +1,20 @@
 import { Category } from "../model/category.schema.js";
-import { cloudinaryUpload } from "../service/cloudinary.js";
+import { cloudinaryDelete, cloudinaryUpload } from "../service/cloudinary.js";
 import ApiErrors from "../utils/ApiErrors.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import { TryCatch } from "../utils/TryCatch.js";
 
 const createCategory = TryCatch(async (req, res) => {
+    let { name, slug } = req.body
     const updates = {};
     if (req.file) {
         const thumbnail = req.file;
         const cloudinaryResult = await cloudinaryUpload(thumbnail.path, name, "category");
         updates.thumbnail = {
-            public_id: cloudinaryResult.public_id,
+            public_id: cloudinaryResult.uploadResult.public_id,
             url: cloudinaryResult.optimizeUrl,
         };
     }
-    let { name, slug } = req.body
     if (!name) {
         throw new ApiErrors(400, "category name is required")
     }
@@ -27,8 +27,47 @@ const createCategory = TryCatch(async (req, res) => {
     }
     updates.name = name
     updates.slug = slug
-    const category = await Category.create({ updates })
+    const category = await Category.create(updates)
     return res.status(201).json(new ApiResponse(201, "category created successfully", { category }))
 })
 
-export { createCategory }
+const updateCategory = TryCatch(async (req, res) => {
+    let { name, slug } = req.body
+    const { categoryName } = req.params
+    const category = await Category.findOne({ name: categoryName })
+    if (!category) {
+        throw new ApiErrors(400, "no category found")
+    }
+    const updates = {};
+    if (req.file) {
+        const thumbnail = req.file;
+        if (category.thumbnail.public_id) {
+            await cloudinaryDelete(category.thumbnail.public_id);
+        }
+        const cloudinaryResult = await cloudinaryUpload(thumbnail.path, category.name, "category");
+        console.log("asas");
+        updates.thumbnail = {
+            public_id: cloudinaryResult.uploadResult.public_id,
+            url: cloudinaryResult.optimizeUrl,
+        };
+    }
+    if (!name) {
+        throw new ApiErrors(400, "category name is required")
+    }
+    const isName = await Category.findOne({ name })
+    if (isName) {
+        throw new ApiErrors(400, "category name must be unique")
+    }
+    if (!slug) {
+        slug = name.replaceAll(" ", "-").toLowerCase()
+    }
+    updates.name = name
+    updates.slug = slug
+    const updateedCategory = await Category.findOneAndUpdate({ name: category.name }, { $set: updates }, { new: true })
+    return res.status(201).json(new ApiResponse(201, "category created successfully", { categroy: updateedCategory }))
+})
+
+
+
+
+export { createCategory, updateCategory }
--- src/route/category.route.js
+++ src/route/category.route.js
@@ -3,10 +3,11 @@ import express from "express";
 import { upload } from "../middleware/multer.middleware.js";
 import { auth } from "../middleware/auth.middleware.js";
 import { admin } from "../middleware/admin.middleware.js";
-import { createCategory } from "../controller/category.controller.js";
+import { createCategory, updateCategory } from "../controller/category.controller.js";
 const router = express.Router()
 
 router.route("/categories").post(auth, admin, upload.single("thumbnail"), createCategory)
+router.route("/categories/:categoryName").put(auth, admin, upload.single("thumbnail"), updateCategory)
 
 
 export default router
