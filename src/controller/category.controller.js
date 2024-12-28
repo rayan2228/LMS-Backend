8942da65e5fc50df9ea70706c65f8af7c4a5425e
rayan2228 <rayanh.dev@gmail.com>
2024-12-28 16:24:32 +0600

    create category controller with thumbnail; uplod

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -1,21 +1,33 @@
 import { Category } from "../model/category.schema.js";
+import { cloudinaryUpload } from "../service/cloudinary.js";
 import ApiErrors from "../utils/ApiErrors.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import { TryCatch } from "../utils/TryCatch.js";
 
 const createCategory = TryCatch(async (req, res) => {
+    const updates = {};
+    if (req.file) {
+        const thumbnail = req.file;
+        const cloudinaryResult = await cloudinaryUpload(thumbnail.path, name, "category");
+        updates.thumbnail = {
+            public_id: cloudinaryResult.public_id,
+            url: cloudinaryResult.optimizeUrl,
+        };
+    }
     let { name, slug } = req.body
     if (!name) {
         throw new ApiErrors(400, "category name is required")
-    }
-    if (!slug) {
-        slug = name.replaceAll(" ", "-").toLowerCase()
     }
     const isName = await Category.findOne({ name })
     if (isName) {
         throw new ApiErrors(400, "category name must be unique")
     }
-    const category = await Category.create({ name, slug })
+    if (!slug) {
+        slug = name.replaceAll(" ", "-").toLowerCase()
+    }
+    updates.name = name
+    updates.slug = slug
+    const category = await Category.create({ updates })
     return res.status(201).json(new ApiResponse(201, "category created successfully", { category }))
 })
 
