4dd1549e812b6e154e162fdb52c4f35cc69cf67e
rayan2228 <rayanh.dev@gmail.com>
2025-02-01 16:59:46 +0600

    feat: add order schema for managing user orders and payment details

---
+++ src/model/order.schema.js
@@ -0,0 +1,18 @@
+import mongoose, { Schema, model } from "mongoose";
+const orderSchema = new Schema(
+    {
+        user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+        cart: { type: Schema.Types.ObjectId, ref: "Cart", required: true },
+        totalPrice: { type: Number, required: true },
+        status: {
+            type: String,
+            enum: ["pending", "processing", "delivered", "cancelled"],
+            default: "pending",
+        },
+        paymentMethod: { type: String, enum: ["stripe", "paypal", "sslCommerz"], required: true },
+        paymentStatus: { type: String, enum: ["pending", "paid", "failed"], default: "pending" },
+    },
+    { timestamps: true }
+);
+
+export const Order = mongoose.models.Order || model("Order", orderSchema);
